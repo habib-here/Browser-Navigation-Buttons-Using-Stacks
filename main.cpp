a15b1c4d3e80e4aedd451157b500aebe9ed9f805
Habib <habibahmedmalik1@gmail.com>
2024-10-24 19:27:34 +0500

    Final commit for no.

--- main.cpp
+++ main.cpp
@@ -4,9 +4,13 @@ using namespace std;
 
 int main(void)
 {
-    stack t1;
-    t1.pop();
-    t1.clear();
-    cout << "Working !" << endl;
+    NavigationButtons b1;
+    b1.visit("https://example.com");
+    b1.visit("https://example.com/page1");
+    b1.visit("https://example.com/page2");
+
+    b1.back();
+    b1.forward();
+
     return 0;
 }
